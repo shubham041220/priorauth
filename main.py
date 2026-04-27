b536a9c3517c0b45a30d755d06de01fb81d1e179
shubham041220 <sharmashubham683@gmail.com>
2026-04-27 19:41:18 +0530

    updating latest files
    updating latest files

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from graphbuilder import build_graph
 if __name__ == "__main__":
 
     graph = build_graph()
+    graph_obj = graph.get_graph()
 
+    print(graph_obj.draw_mermaid())
     result = graph.invoke({})
 
     print("\nFINAL RESULT\n")
